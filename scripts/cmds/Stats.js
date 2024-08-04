c206f0f17916dda3a30a63860d824404e1a6f293
Dragunov226 <abdallahkabore683@gmail.com>
2024-08-04 15:25:54 +0000

    Create Stats.js

---
+++ scripts/cmds/Stats.js
@@ -0,0 +1,70 @@
+const fast = require('fast-speedtest-api');
+
+module.exports = {
+  config: {
+    name: "stats",
+    aliases: ["upt"],
+    version: "1.5",
+    author: "S",
+    role: 2,
+    shortDescription: {
+      en: "Uptime"
+    },
+    longDescription: {
+      en: "Shows uptime, speed and ping functionalities."
+    },
+    category: "system",
+    guide: {
+      en: "Use {p}allinone to see combined stats, speed test, and ping."
+    }
+  },
+  onStart: async function ({ api, event, usersData, threadsData }) {
+    try {
+      // Uptime Calculation
+      const uptime = process.uptime();
+      const hours = Math.floor(uptime / 3600);
+      const minutes = Math.floor((uptime % 3600) / 60);
+      const seconds = Math.floor(uptime % 60);
+      const uptimeString = `${hours}Hrs ${minutes}min ${seconds}sec`;
+
+      // Speed Test
+      const speedTest = new fast({
+        token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
+        verbose: false,
+        timeout: 10000,
+        https: true,
+        urlCount: 5,
+        bufferSize: 8,
+        unit: fast.UNITS.Mbps
+      });
+
+      const speedResult = await speedTest.getSpeed();
+
+      // Ping
+      const timeStart = Date.now();
+      await api.sendMessage("𝚟𝚘𝚒𝚕𝚊 𝚖𝚊𝚒𝚝𝚛𝚎 🧞‍♂️", event.threadID);
+      const ping = Date.now() - timeStart;
+
+      let pingStatus = "Not smooth throw your router, buddy!";
+      if (ping < 400) {
+        pingStatus = "Smooth like Ferrari!";
+      }
+
+      // Total Users and Threads
+      const allUsers = await usersData.getAll();
+      const allThreads = await threadsData.getAll();
+
+      // Create combined message
+      const combinedMessage =
+        `⏰ Uptime\n 🧞‍♂️ ${uptimeString}\n\n` +
+        `📶 𝚟𝚒𝚝𝚎𝚜𝚜𝚎\n 🧞‍♂️ ${speedResult} MBPS\n\n` +
+        `🛜 Ping\n 🧞‍♂️ ${ping} MS\n\n` +
+        `👥 U𝚝𝚒𝚕𝚒𝚜𝚊𝚝𝚎𝚞𝚛𝚜\n 🧞‍♂️ ${allUsers.length} users\n\n` + `🚀 𝙶𝚛𝚘𝚞𝚙𝚎𝚜\n ⭔ ${allThreads.length} threads\n\n`+`🧞‍♂️ Ping Status ~ ${pingStatus}`;
+
+      api.sendMessage(combinedMessage, event.threadID);
+    } catch (error) {
+      console.error(error);
+      api.sendMessage("An error occurred while retrieving data.", event.threadID);
+    }
+  }
+};
